6ba3575fe36295cff3c226d8a670471261711dcc
Matt and Terra’s Adventures <Mattjhagen@users.noreply.github.com>
2025-06-25 16:33:17 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { CallToAction } from "client/src/components/CallToAction.tsx";
+import { Hero } from "client/src/components/Hero.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Router, Route, Switch } from "wouter";
-import { AuthProvider } from "@/hooks/useAuth";
-import ProtectedRoute from "@/components/ProtectedRoute";
+import { AuthProvider } from "client/src/components/auth/AuthForm.tsx";
+import ProtectedRoute from "client/src/components/ProtectedRoute.tsx";
 import Index from "./components/index/Index";
-import Auth from "./pages/Auth";
+import Auth from ".client/src/hooks/useAuth.tsx";
 import Dashboard from "./components/dashboard/Dashboard";
 import NotFound from "./pages/NotFound";
 
