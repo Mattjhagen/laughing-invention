61b26f327c8235b1053201c6278e82f2f7fea9e0
Matt and Terra’s Adventures <Mattjhagen@users.noreply.github.com>
2025-06-25 17:13:32 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import { CallToAction } from "client/src/components/CallToAction.tsx";
 import { Hero } from "client/src/components/Hero.tsx";
-import { TooltipProvider } from "/src/components/ui/tooltip";
+import { TooltipProvider } from "src/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Router, Route, Switch } from "wouter";
 import { AuthProvider } from "client/src/components/auth/AuthForm.tsx";
 import ProtectedRoute from "client/src/components/ProtectedRoute.tsx";
-import Index from "./components/index/Index";
-import Auth from "./client/src/hooks/useAuth.tsx";
-import Dashboard from "./components/dashboard/Dashboard";
-import NotFound from "./pages/NotFound";
+import Index from "components/index/Index";
+import Auth from "client/src/hooks/useAuth.tsx";
+import Dashboard from "components/dashboard/Dashboard";
+import NotFound from "pages/NotFound";
+import main from "client/src/main.tsx"
 
 const queryClient = new QueryClient();
 
